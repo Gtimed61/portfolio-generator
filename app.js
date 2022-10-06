a43bffbea6548ee18c1a1d3d4e2cd78a89c4c7bc
Thorne <gthorne61@gmail.com>
2022-10-05 19:21:35 -0500

    snapshot

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 
-const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
 
 const profileDataArgs = process.argv.slice(2);
 
+console.log(profileDataArgs);
+
 const [name, github] = profileDataArgs;
+
+console.log(name, gitbub);
+
+const pageHTML = generatePage(name, github);
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
     if (err) throw new Error (err);
